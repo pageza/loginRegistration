8146c15aba7371e0851ec27296b94e8bcbb1221c
pageza <zap.freelance@gmail.com>
2020-09-13 14:51:22 -0600

    finished assignment, no style

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ const UserSchema = new mongoose.Schema({
 // Create Object of Models
 const User = mongoose.model('User', UserSchema)
 
+
+// Setting the static directoy for express
+app.use(express.static(__dirname + '/static'));
 // Setting Express app to accept POST requests
 app.use( express.urlencoded({extended: true}) );
 // Setting up session for the app
@@ -47,6 +50,75 @@ app.set( 'view engine', 'ejs' );
 app.set( 'views', __dirname + '/views' );
 
 // **ROUTES**
+app.get('/', (req, res) => {
+    res.render('index')
+});
+app.post('/register', (req, res) => {
+    User.findOne({email:req.body.email}, (err,user) => {
+        if(user){
+            req.flash('reg', "That email is already in use.")
+            return res.redirect('/')
+        }
+        else {
+            if(req.body.password != req.body.passwordConfirm){
+                req.flash('reg', "Passwords do not match")
+                return res.redirect('/')
+            }
+        }
+        bcrypt.hash(req.body.password, 10)
+            .then(hashed_password => {
+                const char = req.body.password.search(/[a-z]/);
+                const num = req.body.password.search(/[0-9]/);
+                const upperChar = req.body.password.search(/[A-Z]/);
+                const length = req.body.password.length;
+                if(char<0 || num<0 || upperChar<0){
+                    req.flash('reg', "Password requires at least one uppercase letter and one number")
+                    hashed_password = req.body.password;
+                    req.session.errors = 1;
+                }
+                if(length < 8){
+                    hashed_password = req.body.password;
+                }
+                newUser = new User({first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, birthday: req.body.birthday, password: hashed_password})
+                newUser.validate((err) => {
+                    if(err){
+                        for(let key in err.errors){
+                            req.flash('reg', err.errors[key].message);
+                        }
+                        res.redirect('/')
+                    }
+                    else if(req.session.errors){
+                        console.log(req.session.errors);
+                        res.redirect('/');
+                    }
+                    else {
+                        newUser.save(err => {
+                            req.session.user_id = newUser.id;
+                            res.redirect('/success/'+newUser.id);
+                        });
+                    }
+                });
+            })
+            .catch(err => res.redirect('/'))
+    })
+});
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err,user) => {
+        if(user){
+            bcrypt.compare(req.body.password, user.password)
+                .then(req.session.email = user.email, req.session.name = user.first_name, res.render('success', {user:req.session}))
+                .catch(err => {res.json(err)})
+        }
+        else {
+            req.flash('reg', "There is no user with those credentials.")
+            res.redirect('/')
+        }
+    })    
+});
+app.post('/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+});
 
 // Setting the app to listen on specified port
  app.listen(PORT, () => console.log("listening on port: ", PORT) );
